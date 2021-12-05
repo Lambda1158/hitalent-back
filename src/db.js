aa7b764bda4fe1b79b217e9165886f74b8bf6e01
lambda1158 <brunoherrera1158@gmail.com>
2021-12-04 21:38:16 -0300

    ahi va

--- src/db.js
+++ src/db.js
@@ -9,15 +9,34 @@ const proConfig = process.env.DATABASE_URL;
 const devConfig=`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${PG_DATABASE}`
 
 
-
-
-const sequelize = new Sequelize(process.env.NODE_ENV === "production" ? proConfig : devConfig,{
-    dialect: 'postgres',
-    protocol: 'postgres',
-    dialectOptions: {},
-    logging: false, // set to console.log to see the raw SQL queries
-    native: false, // lets Sequelize know we can use pg-native for ~30% more speed
-  });
+let sequelize =
+  process.env.NODE_ENV === "production"
+    ? new Sequelize({
+      database: PG_DATABASE,
+      dialect: "postgres",
+      host: DB_HOST,
+      port: 5432,
+      username: DB_USER,
+      password: DB_PASSWORD,
+      pool: {
+        max: 3,
+        min: 1,
+        idle: 10000,
+      },
+      dialectOptions: {
+        ssl: {
+          require: true,
+          // Ref.: https://github.com/brianc/node-postgres/issues/2009
+          rejectUnauthorized: false,
+        },
+        keepAlive: true,
+      },
+      ssl: true,
+    })
+    : new Sequelize(
+      `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${PG_DATABASE}`,
+      { logging: false, native: false }
+    );
 
 
 const basename = path.basename(__filename);

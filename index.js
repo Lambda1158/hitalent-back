9bb2ea961864afff43139c3c8e6f6c4deaa39500
lambda1158 <brunoherrera1158@gmail.com>
2021-12-15 20:50:35 -0300

    aver

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const { Server } = require("socket.io");
 const httpServer = createServer(app);
 const io = new Server(httpServer, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: "https://hitalent-846dd.web.app/",
   },
 });
 
